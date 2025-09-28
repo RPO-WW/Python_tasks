2f5e49540031f8965343e00473cfbc27b7f2cb70
ar15 <george2748@mail.ru>
2025-09-28 09:54:06 +0300

    он принимает файл

--- block11/task169.py
+++ block11/task169.py
@@ -1,18 +1,31 @@
 def move_first_to_last(arr):
+    if not isinstance(arr, list):
+        raise TypeError('arr must be a list')
+
     if len(arr) <= 1:
         return arr
 
-    first_element = arr[0]
-
-    for i in range(len(arr) - 1):
-        arr[i] = arr[i + 1]
-
-    arr[-1] = first_element
-
+    first_element = arr.pop(0)
+    arr.append(first_element)
     return arr
 
 
-arr = [list(map(int, input("ВВедите массив")))]
-print("До:", arr)
-move_first_to_last(arr)
-print("После:", arr)
+def parse_input_line(s: str):
+    s = s.strip()
+    if not s:
+        return []
+    # allow separators: spaces and/or commas
+    parts = [p for part in s.split(',') for p in part.split()]
+    return [int(p) for p in parts]
+
+
+if __name__ == '__main__':
+    try:
+        line = input('Введите массив (например: 1 2 3 4 или 1,2,3,4): ')
+        arr = parse_input_line(line)
+    except ValueError:
+        print('Ошибка: ввод должен содержать целые числа, разделённые пробелами или запятыми.')
+    else:
+        print('До:', arr)
+        move_first_to_last(arr)
+        print('После:', arr)
